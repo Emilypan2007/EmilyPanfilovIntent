ae3572f4bd45a1e342e861a3dc7337acff12aa56
DESKTOP-LKCI9L4 <emily.pn@sisma.org.il>
2024-03-27 14:58:30 +0200

    design

--- app/src/main/java/com/example/emilypanfilovintent/MainActivity.java
+++ app/src/main/java/com/example/emilypanfilovintent/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.emilypanfilovintent;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button plus,sub,multiplication,division,equal,delete,credits;
+    EditText display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        plus=(Button)findViewById(R.id.plus);
+        sub=(Button)findViewById(R.id.sub);
+        multiplication=(Button)findViewById(R.id.multiplication);
+        division=(Button)findViewById(R.id.division);
+        equal=(Button)findViewById(R.id.equal);
+        delete=(Button)findViewById(R.id.delete);
+        credits=(Button)findViewById(R.id.credits);
+        display=(EditText) findViewById(R.id.display);
+        plus.setBackgroundColor(Color.rgb(255,130,250));
+        sub.setBackgroundColor(Color.rgb(255,130,250));
+        multiplication.setBackgroundColor(Color.rgb(255,130,250));
+        division.setBackgroundColor(Color.rgb(255,130,250));
+        delete.setBackgroundColor(Color.rgb(255,130,250));
+        equal.setBackgroundColor(Color.rgb(255,130,250));
+        credits.setBackgroundColor(Color.rgb(255,130,250));
+
     }
 }
